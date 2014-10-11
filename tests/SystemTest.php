45cad5b7b36c95c25c897e5181f74e36386e9033
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2014-10-11 17:17:34 +0300

    Added some unit tests

---
+++ tests/SystemTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace PhakeBuilder\Tests;
+use PhakeBuilder\System;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'System.php';
+
+class SystemTest extends \PHPUnit_Framework_TestCase {
+
+	public function test_getDefaultValue() {
+		// Known value
+		$result = System::getDefaultValue('GIT_BRANCH');
+		$this->assertEquals('master', $result);
+
+		// Unknown value
+		$result = System::getDefaultValue('THIS_VALUE_WILL_NEVER_EXIST');
+		$this->assertNull($result);
+	}
+
+	/**
+	 * @expectedException \RuntimeException
+	 */
+	public function test_doShellCommand_exception() {
+		$result = System::doShellCommand('THIS_COMMAND_WILL_NEVER_EXIST 2>/dev/null');
+	}
+
+	public function test_doShellCommand() {
+		$result = System::doShellCommand('ls');
+		$this->assertFalse(empty($result));
+	}
+
+	public function test_secureString() {
+		$result = System::secureString('foobar', 'bar');
+		$this->assertEquals('fooxxx', $result);
+
+		$result = System::secureString('foobar', '');
+		$this->assertEquals('foobar', $result);
+
+		$result = System::secureString('foobar', ['foo','bar']);
+		$this->assertEquals('xxxxxx', $result);
+
+		$result = System::secureString('foobar', 'bar', '*');
+		$this->assertEquals('foo***', $result);
+	}
+}
+?>
