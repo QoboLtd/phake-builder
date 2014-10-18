c9035ac7124f18f0962b72787c6174eaf475fd0f
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2014-10-18 17:44:55 +0300

    Replaced system rm with PHP alternative

--- lib/System.php
+++ lib/System.php
@@ -33,7 +33,6 @@ class System {
 			'SYSTEM_COMMAND_GIT' => '/usr/bin/git',	
 			'SYSTEM_COMMAND_MYSQL' => '/usr/bin/mysql',
 			'SYSTEM_COMMAND_MYSQL_REPLACE' => '/usr/bin/php vendor/interconnectit/search-replace-db/srdb.cli.php',
-			'SYSTEM_COMMAND_RM' => '/usr/bin/rm -r',
 			'SYSTEM_COMMAND_SERVICE' => '/usr/sbin/service',
 			'SYSTEM_COMMAND_SUDO' => '/usr/bin/sudo',
 		);
