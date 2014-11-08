f49d7b8b772b283ceb0d15233d6dd25393dedb89
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2014-11-08 14:41:41 +0200

    Refactored parts of Phakefile into Filesystem lib (issue #24)

--- lib/System.php
+++ lib/System.php
@@ -27,8 +27,6 @@ class System {
 			'DB_ADMIN_USER' => 'root',
 			'DB_ADMIN_PASS' => '',
 
-			'MKDIR_MODE' => 0755,
-
 			'SYSTEM_COMMAND_MYSQL' => '/usr/bin/mysql',
 			'SYSTEM_COMMAND_MYSQL_REPLACE' => '/usr/bin/php vendor/interconnectit/search-replace-db/srdb.cli.php',
 			'SYSTEM_COMMAND_SERVICE' => '/usr/sbin/service',
