df8eabb7337d1ee33fbc87cbc59606ca5f426bd5
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2016-02-23 13:33:33 +0200

    Unit tests for Archive

---
+++ tests/ArchiveTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Phakebuilder\Tests;
+
+class ArchiveTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function archives()
+    {
+        return [
+        ['hello.zip', 'hello.txt'],
+        ['hello.tar.gz', 'hello.txt'],
+        ['hello.tar.bz2', 'hello.txt'],
+        ];
+    }
+
+    /**
+     * @dataProvider archives
+     */
+    public function testExtract($src, $expected)
+    {
+        $srcDir = __DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'Archive' . DIRECTORY_SEPARATOR;
+        $src = $srcDir . $src;
+        
+        $dstDir = sys_get_temp_dir() . DIRECTORY_SEPARATOR;
+        
+        $result = $dstDir . $expected;
+        $expected = $srcDir . $expected;
+        
+        $this->assertFileNotExists($result, "Result file [$result] already exists");
+        \Phakebuilder\Archive::extract($src, $dstDir);
+        $this->assertFileExists($result, "Result file [$result] was not extracted");
+        $this->assertFileEquals($expected, $result, "Extracted file [$result] does not match source file [$expected]");
+        unlink($result);
+    }
+
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testCompress()
+    {
+        $result = \Phakebuilder\Archive::compress('some', 'other');
+    }
+}
