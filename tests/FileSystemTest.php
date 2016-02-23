89459d52ef11577fa9376ae664fcac155bc1ce51
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2016-02-23 14:04:57 +0200

    More tests for Filesystem

--- tests/FileSystemTest.php
+++ tests/FileSystemTest.php
@@ -4,6 +4,29 @@ namespace Phakebuilder\Tests;
 class FileSystemTest extends \PHPUnit_Framework_TestCase
 {
 
+    public function testMakeDir()
+    {
+        $dstDir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'foobar_' . uniqid();
+        $this->assertFileNotExists($dstDir, "Destination directory [$dstDir] already exists");
+        \Phakebuilder\FileSystem::makeDir($dstDir);
+        $this->assertFileExists($dstDir, "Failed to created destination directory [$dstDir]");
+        $this->assertTrue(is_dir($dstDir), "Destination [$dstDir] is not a directory");
+        rmdir($dstDir);
+    }
+
+    public function testRemovePath()
+    {
+        $dstDir = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'foobar_' . uniqid();
+        $dstFile = $dstDir . DIRECTORY_SEPARATOR . 'foobar.txt';
+        mkdir($dstDir);
+        touch($dstFile);
+        $this->assertFileExists($dstDir, "Failed to create destination directory [$dstDir]");
+        $this->assertFileExists($dstFile, "Failed to create destination file [$dstFile]");
+        \Phakebuilder\FileSystem::removePath($dstDir);
+        $this->assertFileNotExists($dstDir, "Failed to remove destination directory [$dstDir]");
+        $this->assertFileNotExists($dstFile, "Failed to remove destination file [$dstFile]");
+    }
+
     public function testChmodDirectory()
     {
         $tmpDir = tempnam(sys_get_temp_dir(), 'phakeTest_');
@@ -18,7 +41,7 @@ class FileSystemTest extends \PHPUnit_Framework_TestCase
 
         rmdir($tmpDir);
     }
- 
+
     public function testChmodDirectoryRecursive()
     {
         $tmpDir = tempnam(sys_get_temp_dir(), 'phakeTest_');
@@ -26,7 +49,7 @@ class FileSystemTest extends \PHPUnit_Framework_TestCase
         mkdir($tmpDir);
         // Readable only
         chmod($tmpDir, 0400);
- 
+
         $this->assertFalse(is_writeable($tmpDir), "Directory [$tmpDir] is writeable");
         $this->assertFalse(is_executable($tmpDir), "Directory [$tmpDir] is executable");
         $result = \Phakebuilder\FileSystem::chmodPath($tmpDir, 0700, 0600);
