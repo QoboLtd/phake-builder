48a2d47bfef426f3ac93724e4b88a4d18d2dfe12
Leonid Mamchenkov <leonid@mamchenkov.net>
2016-12-11 15:17:33 +0200

    Sami API documentation improvements
    * Created config directory in `etc/sami/`.
    * Moved default `config.php` to `etc/sami/source.php`.
    * Added additional configuration in `etc/sami/full.php`.
    * Added support to pass either full path or config name to Sami.
    * Improved project name setting to avoid empty values.
    * Improved the flexibility of the `build/` folder location.
    
    The default `source.php` configuration file will result in Sami
    generating the API documentation for folder `./src`.  This API
    documentation will cover both public and private/protected
    methods and properties.
    
    The new `full.php` configuration file will result in Sami
    generating the API documentation for folders `./src` and `./vendor`.
    This API documentation will cover only public methods and
    properties.  Also, the `tests/` folders will be ignored.

--- src/Phakefiles/Sami.php
+++ src/Phakefiles/Sami.php
@@ -2,14 +2,35 @@
 // Sami.php related tasks
 group('sami', function () {
 
-    desc('Update API documentation');
+    desc('Update API documentation (source only)');
     task('update', ':builder:init', ':dotenv:create', function ($app) {
         printSeparator();
-        printInfo("Task: sami:update (Update API documentation)");
+        printInfo("Task: sami:update (Update source API documentation)");
 
         $sami_command = getValue('SYSTEM_COMMAND_SAMI', $app);
         $sami_config = getValue('SAMI_CONFIG', $app);
+        $sami_fullpath = true;
+        if (empty($sami_config)) {
+            $sami_config = 'source';
+            $sami_fullpath = false;
+        }
         $sami = new \PhakeBuilder\Sami($sami_command);
-        doShellCommand($sami->update($sami_config));
+        doShellCommand($sami->update($sami_config, $sami_fullpath));
+    });
+
+    desc('Update API documentation (everything)');
+    task('update-all', ':builder:init', ':dotenv:create', function ($app) {
+        printSeparator();
+        printInfo("Task: sami:update-all (Update all API documentation)");
+
+        $sami_command = getValue('SYSTEM_COMMAND_SAMI', $app);
+        $sami_config = getValue('SAMI_CONFIG', $app);
+        $sami_fullpath = true;
+        if (empty($sami_config)) {
+            $sami_config = 'full';
+            $sami_fullpath = false;
+        }
+        $sami = new \PhakeBuilder\Sami($sami_command);
+        doShellCommand($sami->update($sami_config, $sami_fullpath));
     });
 });
